b3056881639d762fabdceb85f8077fdf4a16eee6
Tim Toyoshima <timtoyoshima@hotmail.com>
2013-02-13 12:39:23 -0800

    fix syntax

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -1,6 +1,6 @@
 execute "mysql-create-table" do
   command "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
-  id UNSIGNED INT NOT NULL AUTO_INCREMENT,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   url VARCHAR(255) NOT NULL,
   caption VARCHAR(255),
   PRIMARY KEY (id)
