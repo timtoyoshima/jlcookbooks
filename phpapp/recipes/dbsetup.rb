44b655cc563b958a9d246d6a8f56251fd92baf26
Tim Toyoshima <timtoyoshima@hotmail.com>
2013-02-13 12:17:39 -0800

    add comma

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -2,7 +2,7 @@ execute "mysql-create-table" do
   command "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
   id UNSIGNED INT NOT NULL AUTO_INCREMENT,
   url VARCHAR(255) NOT NULL,
-  caption VARCHAR(255)
+  caption VARCHAR(255),
   PRIMARY KEY (id)
 )'"
   not_if "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'SHOW TABLES' | grep [:phpapp][:dbtable]"
