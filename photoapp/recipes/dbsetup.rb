c1625a3ea5b5a12ead49931ef0bb52452df0a18f
Tim Toyoshima <timtoyoshima@hotmail.com>
2013-02-14 15:27:30 -0800

    fix transposition

--- photoapp/recipes/dbsetup.rb
+++ photoapp/recipes/dbsetup.rb
@@ -1,11 +1,10 @@
 execute "mysql-create-table" do
-  command "/usr/bin/mysql -u#{node[:deploy][:myphotoapp][:database][:username]} -p#{node[:deploy][:myphotoapp][:database][:password]} #{node[:deploy][:myphotoapp][:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
+  command "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  author VARCHAR(63) NOT NULL,
-  message TEXT,
+  url VARCHAR(255) NOT NULL,
+  caption VARCHAR(255),
   PRIMARY KEY (id)
 )'"
-  not_if "/usr/bin/mysql -u#{node[:deploy][:myphotoapp][:database][:username]} -p#{node[:deploy][:myphotoapp][:database][:password]} #{node[:deploy][:myphotoapp][:database][:database]} -e'SHOW TABLES' | grep [:phpapp][:dbtable]"
+  not_if "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'SHOW TABLES' | grep [:phpapp][:dbtable]"
   action :run
 end
-
